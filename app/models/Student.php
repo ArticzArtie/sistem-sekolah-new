d2cbd672a69974b83973eabcdb0a31dd5010b465
AlbertJamesVictorius <albert.021@ski.sch.id>
2026-04-21 13:09:51 +0700

    feat: implement student create page

--- app/models/Student.php
+++ app/models/Student.php
@@ -6,31 +6,28 @@ use App\Core\Database;
 
 class Student extends Database
 {
-
     protected $table = 'students';
 
-    // Fungsi menampilkan daftar siswa
     public function getStudents()
     {
         $students = [];
 
-        $query = "SELECT * FROM {$this->table}";
+        $query = "SELECT * from {$this->table}";
         $stmt = $this->connection->prepare($query);
         $stmt->execute();
 
         $result = $stmt->get_result();
 
-        while($student = $result->fetch_assoc()) {
+        while ($student = $result->fetch_assoc()) {
             $students[] = $student;
         }
 
         return $students;
     }
 
-    // Fungsi menampilkan data siswa
     public function getStudent(int $id)
     {
-        $query = "SELECT * FROM ($this->table) WHERE id = ?";
+        $query = "SELECT * from {$this->table} where id = ?";
         $stmt = $this->connection->prepare($query);
         $stmt->bind_param('i', $id);
         $stmt->execute();
@@ -41,7 +38,23 @@ class Student extends Database
         return $student;
     }
 
+    public function insert(array $data)
+    {
+        $nis = htmlspecialchars($data['nis']);
+        $name = htmlspecialchars($data['name']);
+        $class = htmlspecialchars($data['class']);
+        $phoneNumber = htmlspecialchars($data['phone_number']);
 
+        $query = "INSERT INTO {$this->table} (nis, name, class, phone_number) VALUES (?, ?, ?, ?)";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("ssss", $nis, $name, $class, $phoneNumber);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            header('Location: /students');
+            exit();
+        } else {
+            die('Error to store students : ' . $stmt->error);
+        }
+    }
 }
-
-?>
